a5ac7abbe6881192b952e117263935984b055134
Angel <angelherrarte3@gmail.com>
2023-01-23 13:43:55 -0600

    Add comments

--- src/main/java/com/stackvectorcalculator/StackVector.java
+++ src/main/java/com/stackvectorcalculator/StackVector.java
@@ -6,27 +6,31 @@ import java.util.Vector;
 public class StackVector<E> implements Stack<E> {
     protected Vector<E> data = new Vector<E>(1);
 
+    // Add new element to top
     @Override
     public void push(E item) {
         data.addElement(item);
-
     }
 
+    // Remove last element pushed and return value
     @Override
     public E pop() throws EmptyStackException {
         return data.remove(data.size() - 1);
     }
 
+    // Return last element pushed
     @Override
     public E peek() throws EmptyStackException {
         return data.lastElement();
     }
 
+    // Return true if stack is empty
     @Override
     public boolean empty() {
         return data.isEmpty();
     }
 
+    // Returns the size of stack
     @Override
     public int size() {
         return data.size();
